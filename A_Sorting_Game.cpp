622308e16b75a58afd884894d760f640c28ddbac
Mohit Garg <mohitgarghnd825@gmail.com>
2026-02-21 23:51:47 +0530

    practice

---
+++ A_Sorting_Game.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int T;
+    cin>>T;
+    while (T--) {
+        int n;
+        string s;
+        cin >> n >> s;
+        string t = s;
+        sort(t.begin(), t.end());
+        if (s == t) {
+            cout << "Bob\n";
+            continue;
+        }
+        vector<int> pos;
+        for (int i = 0; i < n; ++i) if (s[i] != t[i]) pos.push_back(i+1);
+        cout << "Alice\n";
+        cout << pos.size() << "\n";
+        for (int i = 0; i < (int)pos.size(); ++i) {
+            if (i) cout << ' ';
+            cout << pos[i];
+        }
+        cout << "\n";
+    }
+    return 0;
+}
